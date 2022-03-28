456c2e00a297312181448e3732c4d7c5fc556ba0
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-03-28 09:36:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <numeric>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    long long n, k;
+    cin >> n >> k;
+    if(k <= (n + 1)/2){
+        cout << k*2-1 << endl;
+    }
+    else{
+        cout << (k-(n+1)/2)*2 << endl;
+    }
+}
