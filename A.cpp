16dc022468de5d644e6d19dedb80f0a2bc14ac65
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-11-28 01:02:33 +0100

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string> 
+#define ll long long
+using namespace std;
+
+int main(){
+    int w;
+    cin >> w;
+    if(w<=2){
+        cout << "NO" << endl;
+        return 0;
+    }
+    if(w % 2 == 0){
+        cout << "YES" << endl;
+        return 0;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+        return 0;
+
+    return 0;
+}
