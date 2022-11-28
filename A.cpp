47477e178e95470e46ac4a35c354945b7e0b6a16
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-11-28 19:36:43 +0100

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+int main() {
+    int m,n;
+    cin >> m >> n;
+    cout << (m*n)/2 << endl;
+}
+
+
+
+
+
+    
