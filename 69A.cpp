10f02726b61b8f79a5b5b70012383593763ff528
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2023-01-05 17:29:46 -0500

    Create 69A.cpp

---
+++ 69A.cpp
@@ -0,0 +1,21 @@
+int n, x, y, z, xsum(0), ysum(0), zsum(0);
+    cin >> n;
+
+    while (n--)
+    {
+        cin >> x >> y >> z;
+        xsum += x;
+        ysum += y;
+        zsum += z;
+    }
+
+    if (xsum == 0 && ysum == 0 && zsum == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
