40f7934e0cfad40a87717c7fedd4b2b19f074e83
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2023-01-05 01:10:56 -0500

    Create 118A.cpp

---
+++ 118A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    for(int i = 0; i < s.length(); i++){
+        if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y'){
+            s = s.replace(i, 1, "");
+            i -= 1;
+        }
+        
+    }
+    for(int i = 0; i < s.length(); i++){
+        cout << "." << s[i];
+    }
+    
+}
